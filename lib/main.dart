a4d4bda6bb2334116baca49013826b436871ef54
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-02-12 16:04:16 +0700

    build it

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'pages/landing_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                "Hallo, semuanya!", 
-                style: GoogleFonts.poppins(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                "Hallo, semuanya!", 
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ],
-          ),
-        )
-      ),
+      home: LandingPage()
     );
   }
 }
