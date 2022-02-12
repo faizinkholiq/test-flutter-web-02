4dc3e864ab842deddc922d7c8cd61e207d5406d9
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-02-12 16:14:58 +0700

    update gesture / state management

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,109 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Image.asset(
-                'background.png', 
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                fit: BoxFit.fill,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 100,
-                  vertical: 30,
-                ),
-                child: Column(
-                  children: [
-                    // Navigation
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset(
-                          'logo.png',
-                          width: 72,
-                          height: 40,
-                        ),
-                        Row(
-                          children: [
-                            Submenu(text: "Guides", active: true,),
-                            SizedBox(width: 50,),
-                            Submenu(text: "Pricing", active: false,),
-                            SizedBox(width: 50,),
-                            Submenu(text: "Team", active: false,),
-                            SizedBox(width: 50,),
-                            Submenu(text: "Stories", active: false,),
-                          ]
-                        ),
-                        Image.asset(
-                          'btn.png',
-                          width: 163,
-                          height: 53,
-                        ),
-                      ],
-                    ),
-                    // Content
-                    SizedBox(height: 76),
-                    Image.asset("illustration.png"),
-                    // Footer
-                    SizedBox(height: 84,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset('Icon_down_solid.png', width: 24,),
-                        SizedBox(height: 13,),
-                        Text('Scroll to learn more', style: GoogleFonts.poppins(fontSize: 20, color: Colors.black)),
-                      ]
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        )
-    );
-  }
+  State<LandingPage> createState() => _LandingPageState();
 }
 
-class Submenu extends StatelessWidget {
-  String text;
-  bool active;
-
-  Submenu({
-    required this.text,
-    required this.active,
-  });
+class _LandingPageState extends State<LandingPage> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          text,
-          style: GoogleFonts.poppins(
-            fontSize: 18,
-            color: Color(0xff1D1E3C),
-            fontWeight: active? FontWeight.w500 : FontWeight.w300,
+    Widget navItem(String title, int index) {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              color: Color(0xff1D1E3C),
+              fontWeight:
+                  index == selectedIndex ? FontWeight.w500 : FontWeight.w300,
+            ),
           ),
-        ),
-        Container(
-          width: 66,
-          height: 2,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: active? Color(0xffFE998D) : Colors.transparent,
-          )
-        )
-      ]
-    );
+          Container(
+              width: 66,
+              height: 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: index == selectedIndex
+                    ? Color(0xffFE998D)
+                    : Colors.transparent,
+              ))
+        ]),
+      );
+    }
+
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Stack(
+        children: [
+          Image.asset(
+            'background.png',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.fill,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 100,
+              vertical: 30,
+            ),
+            child: Column(
+              children: [
+                // Navigation
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'logo.png',
+                      width: 72,
+                      height: 40,
+                    ),
+                    Row(children: [
+                      navItem("Guides", 0),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      navItem("Pricing", 1),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      navItem("Team", 2),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      navItem("Stories", 3),
+                    ]),
+                    Image.asset(
+                      'btn.png',
+                      width: 163,
+                      height: 53,
+                    ),
+                  ],
+                ),
+                // Content
+                SizedBox(height: 76),
+                Image.asset("illustration.png"),
+                // Footer
+                SizedBox(
+                  height: 84,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset(
+                    'Icon_down_solid.png',
+                    width: 24,
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                  Text('Scroll to learn more',
+                      style: GoogleFonts.poppins(
+                          fontSize: 20, color: Colors.black)),
+                ])
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
